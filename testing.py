3d6362ae67be172b8016a3ee5a2b9ffd0c4b01d3
Montserrat Molina <mgmolina3@miners.utep.edu>
2023-11-14 20:33:30 -0700

    added testing logic for all algorithms

--- testing.py
+++ testing.py
@@ -8,33 +8,28 @@ def main():
     statistics = getCombinations()
 
     for item in statistics:
-        print(item)
-    
         algo1 = item[1]
         param1 = item[2]
         player1 = item[3]
-        
+
         algo2 = item[5]
         param2 = item[6]
-        player2= item[7]
+        player2 = item[7]
         
-        gameOutcome = playGame(algo1=algo1, param1=param1, player1=player1, algo2=algo2, param2=param2, player2=player2)
-        print(gameOutcome)
-        
-        if gameOutcome == 0: # draw
-            item[9] += 1
-        elif gameOutcome == -1: # red won
-            item[4] += 1
-        else: # yellow won
-            item[8] += 1
-        
+        for _ in range(100):
+            gameOutcome = playGame(algo1=algo1, param1=param1, player1=player1, algo2=algo2, param2=param2, player2=player2)            
+            if gameOutcome == 0: # draw
+                item[9] += 1
+            elif gameOutcome == -1: # red won
+                item[4] += 1
+            else: # yellow won
+                item[8] += 1
+
         print(item)
-        break
 
 def getCombinations():
-    # algorithms = [["UR", 0], ["DLMM", 5], ["PMCGS500", 500], ["PMCGS10000", 10000], ["UCT500", 500], ["UCT10000", 10000]]
-    algorithms = [["PMCGS500", 500], ["PMCGS10000", 10000], ["UCT500", 500], ["UCT10000", 10000]]
-    # algorithms = ["UR", "DLMM", "PMCGS500", "PMCGS10000", "UCT500", "UCT10000"]
+    algorithms = [["UR", 0], ["DLMM", 5], ["PMCGS500", 500], ["PMCGS1000", 1000], ["UCT500", 500], ["UCT1000", 1000]]
+    # algorithms = [["PMCGS500", 500], ["PMCGS10000", 1000], ["UCT500", 500], ["UCT10000", 1000]]
     statistics = []
     # make all 36 combinations of algorithms that we want to test
     for i in range(len(algorithms)):
@@ -56,56 +51,38 @@ def playGame(algo1, param1, player1, algo2, param2,  player2) -> str:
 
     while game.gameOutcome is None:
         # Algorithm 1's turn
-        if "PMCGS" in algo1:
+        if algo1 == "UR":
+            # TODO
+            pass
+        elif algo1 == "DLMM":
+            # TODO
+            pass
+        elif "PMCGS" in algo1:
             move = monteCarloTreeSearch(game, param1, player1, printMode)
-            print("Before reset:")
-            game.printBoard()
-            game.resetToOriginalState()
-            print("After reset:")
-            game.printBoard()
-            
-            game.checkGameStatus(move.coordinates, player1)
-            if game.gameOutcome == -1:
-                print("Winning move: ", move.coordinates)
-            # if game.gameOutcome == 0:  # draw
-            #     return "Draw", None
-            # if wasWinningMove(game.gameOutcome, player1):
-            #     return algo1, player1
-
         elif "UCT" in algo1:
             move = monteCarloTreeSearch(game, param1, player1, printMode, True)
-            # if game.gameOutcome == 0:  # draw
-            #     return "Draw", None
-            # if wasWinningMove(game.gameOutcome, player1):
-            #     return algo1, player1
+        
+        game.resetToOriginalState()
+        game.checkGameStatus(move.coordinates, player1)
 
         if game.gameOutcome is not None:
             return game.gameOutcome
         
         # Algorithm 2's turn
-        if "PMCGS" in algo2:
+        if algo2 == "UR":
+            # TODO
+            pass
+        elif algo2 == "DLMM":
+            # TODO
+            pass
+        elif "PMCGS" in algo2:
             move = monteCarloTreeSearch(game, param2, player2, printMode)
-            print("Before reset:")
-            game.printBoard()
-            game.resetToOriginalState()
-            print("After reset:")
-            game.printBoard()
-            game.checkGameStatus(move.coordinates, player2)
-            if game.gameOutcome == 1:
-                print("Winning move: ", move.coordinates)
-            # if game.gameOutcome == 0:  # draw
-            #     return "Draw", None
-            # if wasWinningMove(gameStatus, player2):
-            #     return algo2, player2
-
         elif "UCT" in algo2:
             move = monteCarloTreeSearch(game, param2, player2, printMode, True)
-            # if gameStatus == 0:  # draw
-            #     return "Draw", None
-            # if wasWinningMove(gameStatus, player2):
-            #     return algo2, player2
         
-    
+        game.resetToOriginalState()
+        game.checkGameStatus(move.coordinates, player2)
+        
     return game.gameOutcome
 
 def getEmptyBoard():
@@ -125,11 +102,6 @@ def wasWinningMove(gameStatus, nextMovePlayer) -> bool:
     if (gameStatus == -1 and nextMovePlayer == "R") or (gameStatus == 1 and nextMovePlayer == "Y"):
         return True
     return False
-
-def printBoard(board):
-    for row in board:
-        print(row)
-
 
 if __name__ == "__main__":
     main()
